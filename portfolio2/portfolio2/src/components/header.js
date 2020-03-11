dbc0c9c4f95f4da1c19cb41ba02052f580898af2
Ariana Hwang <ajhwang0717@gmail.com>
2020-03-11 19:32:04 -0400

    feature: added header and modified stylesheet

--- portfolio2/portfolio2/src/components/header.js
+++ portfolio2/portfolio2/src/components/header.js
@@ -1,8 +1,22 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 const Header = () => (
     <header>
-        <p>header</p>
+        <div className='container'>
+            <div className='inner-header'>
+                <div className='logo'>
+                    <Link to='/'>Ariana Hwang</Link>
+                </div>
+                <div className='navigation'>
+                    <nav>
+                        <Link to='/about'>About Me</Link>
+                        <Link to='/projects'>Projects</Link>
+                        <Link to='/contact'>Contact</Link>
+                    </nav>
+                </div>
+            </div>
+        </div>
     </header>
 )
 
